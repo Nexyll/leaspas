bd00dd278feedd518bd83d194170a5167c32f7ae
MonsieurMan <CodingJuice@gmail.com>
2016-10-25 01:24:57 +0200

    Add eleve

---
+++ LEASPAS/Model/Eleve.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using LEASPAS.Model;
+
+namespace LEASPAS
+{
+    class Eleve
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public char Sexe { get; set; }
+        public List<Motif> Motifs { get; } = new List<Motif>();
+        public Niveau Niveau { get; set; } 
+        public Origine Origine { get; set; }
+        public Etablissement Etablissement { get; set; }
+
+        public Eleve(string nom, string prenom, char sexe, Motif motif)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Sexe = sexe;
+            Motifs.Add(motif);
+        }
+    }
+}

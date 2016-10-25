fd3d25f18b5997a26d0eca8138bfc17a105f4299
Nexyll <antoine7104@gmail.com>
2016-10-25 03:04:22 +0200

    NIL

---
+++ LEASPAS/DAO/DAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace LEASPAS.DAO {
+    public abstract class DAO {
+        public SQLiteConnection Connection { get; set; }
+
+        protected DAO(SQLiteConnection connection)
+        {
+            Connection = connection;
+        }
+    }
+}

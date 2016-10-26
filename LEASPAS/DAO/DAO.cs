0a5cfef9b088c690fd2c7825ab88e579d306f53a
Nexyll <antoine7104@gmail.com>
2016-10-26 02:04:26 +0200

    Implémentation de la classe abstraite DAO et du singleton pour la connection à la base de données

--- LEASPAS/DAO/DAO.cs
+++ LEASPAS/DAO/DAO.cs
@@ -6,12 +6,44 @@ using System.Threading.Tasks;
 using System.Data.SQLite;
 
 namespace LEASPAS.DAO {
-    public abstract class DAO {
-        public SQLiteConnection Connection { get; set; }
+    public abstract class DAO<T>
+    {
+        /// <summary>
+        /// Connection à la base SQLite pour éffectuer les transaction
+        /// <remarks>À terme, le pattern de singleton pour gérer la connection
+        /// sera à remettre en cause (particulièrement parce qu'il n'est pas
+        /// Thread Safe)</remarks>
+        /// </summary>
+        public SQLiteConnection Connection { get; set; } = DatabaseConnection.GetInstance();
+        /// <summary>
+        /// Permet de créer un objet dans la table lui correspondant, 
+        /// ainsi que dans les tables éventuellements associées à ses 
+        /// relations.
+        /// </summary>
+        /// <param name="obj">Objet à ajouter dans la base de données</param>
+        public abstract void Create(T obj);
 
-        protected DAO(SQLiteConnection connection)
-        {
-            Connection = connection;
-        }
+        /// <summary>
+        /// Permet de trouver l'entrée (dans la base associé au type)
+        /// qui porte l'ID passé en paramètre.
+        /// </summary>
+        /// <param name="ID">ID de l'objet à trouver dans la base de données</param>
+        /// <returns>null si l'objet n'a pas été trouver (oui c'est mal...)
+        /// et construit une instance si l'objet existe dans la base</returns>
+        public abstract T Find(int ID);
+
+        /// <summary>
+        /// Permet de mettre à jour l'objet passé en paramètre dans la
+        /// base de données.
+        /// </summary>
+        /// <param name="obj">Objet à mettre à jour dans la base de données</param>
+        public abstract void Update(T obj);
+
+        /// <summary>
+        /// Permet de supprimer l'objet passé en paramètre dans la base
+        /// de données
+        /// </summary>
+        /// <param name="obj">Objet à supprimer dans la base de données</param>
+        public abstract void Delete(T obj);
     }
 }
